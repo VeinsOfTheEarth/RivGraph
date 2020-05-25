dfcea04e24fb6c33b01582167095a66accd71cc5
Jon Schwenk <jonschwenk@gmail.com>
2020-05-25 00:03:40 -0600

    trying to get conda-build to work

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="rivgraph",
-    packages=['rivgraph', 'rivgraph/deltas', 'rivgraph/rivers'],
+    packages=['rivgraph'],
     version="0.2",
     description="Tools for topological analysis of fluvial networks from binary masks",
     author='Jon Schwenk',
     author_email='jonschwenk@gmail.com',
     url='https://github.com/jonschwenk/rivgraph',
     keywords=['deltas', 'mask', 'topology', 'networks'],
-#    install_requires=['pyinstrument_cext>=0.2.0'],
+    install_requires=['setuptools-git'],
     include_package_data=True,
     zip_safe=False
 )
