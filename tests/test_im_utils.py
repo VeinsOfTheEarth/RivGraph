db89c658575fe3ac7034d2b5ec8442eb3e908dc8
JayH <jayaram.hariharan@utexas.edu>
2020-05-30 19:29:19 -0400

    some more im_utils tests

--- tests/test_im_utils.py
+++ tests/test_im_utils.py
@@ -153,6 +153,18 @@ class TestNeighbors_Flat:
         assert np.all(idcs == [18, 19, 23])
         assert np.all(vals == [0., 1., 0.])
 
+    def test_neighbors_bottomwrow(self):
+        I = np.zeros((20,10))
+        I[-1,:] = 2.0
+        Iflat = np.ravel(I)
+        ncols = 10
+        idx = 190
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [180, 181, 191])
+        assert np.all(vals == [0., 0., 2.])
+
 class TestNeighbor_Vals:
     """
     Tests for the various conditions associated with the
@@ -349,3 +361,162 @@ class TestDilate:
                                       [0., 1., 1., 1., 1., 1., 0., 0.],
                                       [0., 0., 0., 1., 0., 0., 0., 0.],
                                       [0., 0., 0., 0., 0., 0., 0., 0.]]))
+
+def test_regionprops():
+    I = np.zeros((3,3))
+    I[1,1] = 1.0
+    props = ['centroid','mean','perim_len','perimeter','convex_area',
+             'eccentricity','equivalent_diameter','major_axis_length',
+             'minor_axis_length']
+    info = im_utils.regionprops(I,props)
+    # make bunch of assertions
+    assert [*info] == props
+    assert np.all(info['centroid']==[1.,1.])
+    assert info['mean']==1.
+    assert info['perim_len']==0.
+    assert np.all(info['perimeter']==[1.,1.])
+    assert info['convex_area']==1
+    assert info['eccentricity']==0
+    assert info['equivalent_diameter']==pytest.approx(1.12837917)
+    assert info['major_axis_length']==0.
+    assert info['minor_axis_length']==0.
+
+def test_crop_binary_im():
+    I = np.zeros((5,5))
+    I[0,0]=1; I[1,2:5]=1;
+    Icrop, pads = im_utils.crop_binary_im(I)
+    # assertions
+    assert np.shape(Icrop) == (2,5)
+    assert np.all(pads==[0, 0, 0, 3])
+
+def test_fill_holes():
+    I = np.ones((3,3))
+    I[1,1] = 0.
+    I_filled = im_utils.fill_holes(I, maxholesize=0)
+    # assertion that hole was filled
+    assert I_filled[1,1] == 1
+
+def test_downsample():
+    I = np.zeros((10,10))
+    I[0:5,0:5] = 1
+    Iout = im_utils.downsample_binary_image(I, [2,2], thresh=0.5)
+    # expect to create downsampled 2x2 image with 3 zeros and a one value
+    assert np.sum(Iout) == 1
+    assert Iout[0,0] == 1
+
+class TestSkelKernels:
+    """
+    Checks for skel_kernels method
+    """
+
+    def test_case_one(self):
+        out = im_utils.skel_kernels(1)
+        assert np.all(out == np.array([[0, 1, 0],
+                                       [1, 1, 1],
+                                       [0, 0, 0]]))
+
+    def test_case_two(self):
+        out = im_utils.skel_kernels(2)
+        assert np.all(out == np.array([[0, 1, 0],
+                                       [0, 1, 1],
+                                       [0, 1, 0]]))
+    def test_case_three(self):
+        out = im_utils.skel_kernels(3)
+        assert np.all(out == np.array([[0, 0, 0],
+                                       [1, 1, 1],
+                                       [0, 1, 0]]))
+    def test_case_four(self):
+        out = im_utils.skel_kernels(4)
+        assert np.all(out == np.array([[0, 1, 0],
+                                       [1, 1, 0],
+                                       [0, 1, 0]]))
+
+class TestBpKernels:
+    """
+    Checks for bp_kernels method
+    """
+
+    def test_case_one(self):
+        out = im_utils.bp_kernels(1)
+        assert np.all(out == np.array([[1, 1, 0],
+                                       [0, 10, 0],
+                                       [0, 0, 0]]))
+
+    def test_case_two(self):
+        out = im_utils.bp_kernels(2)
+        assert np.all(out == np.array([[0, 1, 1],
+                                       [0, 10, 0],
+                                       [0, 0, 0]]))
+
+    def test_case_three(self):
+        out = im_utils.bp_kernels(3)
+        assert np.all(out == np.array([[0, 0, 1],
+                                       [0, 10, 1],
+                                       [0, 0, 0]]))
+
+    def test_case_four(self):
+        out = im_utils.bp_kernels(4)
+        assert np.all(out == np.array([[0, 0, 0],
+                                       [0, 10, 1],
+                                       [0, 0, 1]]))
+
+    def test_case_five(self):
+        out = im_utils.bp_kernels(5)
+        assert np.all(out == np.array([[0, 0, 0],
+                                       [0, 10, 0],
+                                       [0, 1, 1]]))
+
+    def test_case_six(self):
+        out = im_utils.bp_kernels(6)
+        assert np.all(out == np.array([[0, 0, 0],
+                                       [0, 10, 0],
+                                       [1, 1, 0]]))
+
+    def test_case_seven(self):
+        out = im_utils.bp_kernels(7)
+        assert np.all(out == np.array([[0, 0, 0],
+                                       [1, 10, 0],
+                                       [1, 0, 0]]))
+
+    def test_case_eight(self):
+        out = im_utils.bp_kernels(8)
+        assert np.all(out == np.array([[1, 0, 0],
+                                       [1, 10, 0],
+                                       [0, 0, 0]]))
+
+"""LOOK AT FCT - May be incorrectly written"""
+# def test_trim_idcs():
+#     idcs = np.array([[0,1,2,5],[0,0,1,5]]).T
+#     sizeI = [2,2]
+#     new = im_utils.trim_idcs(sizeI, idcs)
+
+
+class TestSkelBranchpts:
+    """
+    Tests for skel_branchpoints method
+    """
+
+    def test_simple(self):
+        I = np.zeros((5,5))
+        I[0:3,2] = 1
+        I[3,1] = 1; I[3,3] = 1
+        I[4,0] = 1; I[4,4] = 1
+        Ipbs = im_utils.skel_branchpoints(I)
+        assert np.all(Ipbs == np.array([[0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0],
+                                        [0, 0, 1, 0, 0],
+                                        [0, 0, 0, 0, 0],
+                                        [1, 0, 0, 0, 1]]))
+
+class TestSkelCurvature:
+    """
+    Tests for skel_pixel_curvature method
+    """
+
+    def test_simplecurves(self):
+        I = np.zeros((5,5))
+        # 90 degree bend
+        I[0:3,2] = 1
+        I[3,0:3] = 1
+        curv = im_utils.skel_pixel_curvature(I)
+        assert curv[3,2] == 90.
