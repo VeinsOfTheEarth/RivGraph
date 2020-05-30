ccd512ba6f6cdb5d315b3fc81b2a9d0450be6f9e
JayH <jayaram.hariharan@utexas.edu>
2020-05-30 13:36:09 -0400

    geo_utils tests, beginnings of im_utils tests

---
+++ tests/test_geo_utils.py
@@ -0,0 +1,94 @@
+import pytest
+import sys, os
+import numpy as np
+import matplotlib.pyplot as plt
+sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+from rivgraph import geo_utils
+import osr, ogr, gdal
+
+def test_getEPSG_fromshp():
+        shp_file = 'tests/data/Colville/Colville_inlet_nodes.shp'
+        epsg = geo_utils.get_EPSG(shp_file, returndict=True)
+        print(epsg)
+        # make assertion
+        assert type(epsg) == dict
+        assert len(epsg.keys()) == 1
+        assert epsg['init'] == 'epsg:32606'
+
+def test_geotiff_vals_from_coords():
+    # read in origin point
+    coords = np.array([[336885.0000000000000000, 7826415.0000000000000000]])
+    gobj = gdal.Open('tests/data/Colville/Colville_islands_filled.tif')
+    vals = geo_utils.geotiff_vals_from_coords(coords,gobj)
+    # assert that value at origin is 0, this is what it is in the mask
+    assert vals[0] == 0
+
+def test_coords_to_xy():
+    # use corners or extents of the geotiff to test
+    xs = [336885.0000000000000000, 383085.0000000000000000]
+    ys = [7780215.0000000000000000, 7826415.0000000000000000]
+    gobj = gdal.Open('tests/data/Colville/Colville_islands_filled.tif')
+    gt = gobj.GetGeoTransform()
+    vals = geo_utils.coords_to_xy(xs, ys, gt)
+    # make assertions
+    assert np.all(vals[0] == [0, 1540])
+    assert np.all(vals[1] == [1540, 0])
+
+def test_transform_coords():
+    xs = [336885.0000000000000000, 383085.0000000000000000]
+    ys = [7780215.0000000000000000, 7826415.0000000000000000]
+    input_EPSG = 32606
+    output_EPSG = 4326
+    xy = geo_utils.transform_coords(xs, ys, input_EPSG, output_EPSG)
+    # make assertions
+    assert np.all(xy[0]==pytest.approx([-151.2921657688682,
+                                        -150.1418733861601]))
+    assert np.all(xy[1]==pytest.approx([70.07706865567432,
+                                        70.51576941571783]))
+
+def test_transform_coodinates():
+    xs = [336885.0000000000000000, 383085.0000000000000000]
+    ys = [7780215.0000000000000000, 7826415.0000000000000000]
+    input_EPSG = 32606
+    output_EPSG = 4326
+    xy = geo_utils.transform_coordinates(xs, ys, input_EPSG, output_EPSG)
+    # make assertions
+    assert np.all(xy[0]==pytest.approx([-151.2921657688682,
+                                        -150.1418733861601]))
+    assert np.all(xy[1]==pytest.approx([70.07706865567432,
+                                        70.51576941571783]))
+
+def test_notransform_coodinates():
+    xs = [336885.0000000000000000, 383085.0000000000000000]
+    ys = [7780215.0000000000000000, 7826415.0000000000000000]
+    input_EPSG = 32606
+    output_EPSG = 32606
+    xy = geo_utils.transform_coordinates(xs, ys, input_EPSG, output_EPSG)
+    # make assertions
+    assert np.all(xy[0]==pytest.approx(xs))
+    assert np.all(xy[1]==pytest.approx(ys))
+
+def test_crop_geotiff():
+    g_path = 'tests/data/Colville/Colville_islands_filled.tif'
+    outpath = 'tests/results/known/cropped.tif'
+    # run cropping function
+    o_path = geo_utils.crop_geotif(g_path,outpath=outpath)
+    # assert file name and existance
+    assert o_path == outpath
+    assert os.path.isfile(o_path) == True
+
+def test_croppad_geotiff():
+    g_path = 'tests/data/Colville/Colville_islands_filled.tif'
+    outpath = 'tests/results/known/croppedpad.tif'
+    # run cropping function
+    o_path = geo_utils.crop_geotif(g_path,npad=6,outpath=outpath)
+    # assert file name and existance
+    assert o_path == outpath
+    assert os.path.isfile(o_path) == True
+
+def test_delete_files():
+    # delete created files at the end
+    for i in os.listdir('tests/results/known/'):
+        os.remove('tests/results/known/'+i)
+    # check directory is empty
+    assert os.listdir('tests/results/known/') == []
--- tests/test_im_utils.py
+++ tests/test_im_utils.py
@@ -1,6 +1,7 @@
 import pytest
 import sys, os
 import numpy as np
+import matplotlib.pyplot as plt
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 from rivgraph import im_utils
 
@@ -18,10 +19,333 @@ def test_neighbor_xy():
     assert x1 == 3
     assert y1 == 7
 
-def test_erode_square():
-    i_sq = np.zeros((5,5))
-    i_sq[1:4,1:4] = 1
-    i_sq_ero = im_utils.erode(i_sq, n=1, strel='square')
-    assert i_sq_ero[1,1] == 0.
-    assert i_sq_ero[2,2] == 1.
-    assert i_sq_ero[3,3] == 0.
+def test_getarray():
+    idx = [2,2]
+    I = np.zeros((5,5))
+    I[2,2] = 1
+    I[1,1] = 0.5
+    I[3,3] = 1.5
+    size = (3,3)
+    array, row, col = im_utils.get_array(idx,I,size)
+    # make assertions
+    assert array[1,1] == 1.
+    assert array[0,0] == 0.5
+    assert array[2,2] == 1.5
+    assert row == 1
+    assert col == 1
+
+def test_edge_coords():
+    I = np.zeros((3,3))
+    sizeI = np.shape(I)
+    edgepts = im_utils.edge_coords(sizeI, dtype='xy')
+    # make assertions to known edge coordinates in x and y
+    assert np.all(edgepts[0]==[0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 1, 0])
+    assert np.all(edgepts[1]==[0, 1, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0])
+
+def test_imshowpair():
+    I1 = np.array([[0,1,0],[0,1,0]],dtype=int)
+    I2 = np.array([[1,1,0],[0,1,1]],dtype=int)
+    im_utils.imshowpair(I1,I2)
+    plt.draw()
+    plt.close()
+    # if plot can be drawn and closed then pass the test
+    assert True
+
+class TestNeighbors_Flat:
+    """
+    Tests for the various conditions associated with the
+    im_utils.neighbors_flat() function
+    """
+
+    def test_neighbors_flat(self):
+        I = np.zeros((5,5))
+        I[1,1] = 1.0; I[2,2] = 3.0
+        Iflat = np.ravel(I)
+        idx = 11
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [ 5,  6,  7, 10, 12, 15, 16, 17])
+        assert np.all(vals == [0., 1., 0., 0., 3., 0., 0., 0.])
+
+    def test_neighbors_flatsmall(self):
+        I = np.zeros((5,5))
+        I[1,2] = 1.0
+        Iflat = np.ravel(I)
+        idx = 3
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [2, 4, 7, 8, 9])
+        assert np.all(vals == [0., 0., 1., 0., 0.])
+
+    def test_neighbors_flatmid(self):
+        I = np.zeros((5,5))
+        I[1,1] = 1.0
+        Iflat = np.ravel(I)
+        idx = 10
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [ 5,  6, 11, 15, 16])
+        assert np.all(vals == [0., 1., 0., 0., 0.])
+
+    def test_neighbors_midcol(self):
+        I = np.zeros((5,5))
+        I[4,3] = 1.0
+        Iflat = np.ravel(I)
+        idx = 19
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [13, 14, 18, 23, 24])
+        assert np.all(vals == [0., 0., 0., 1., 0.])
+
+    def test_neighbors_flatfirst(self):
+        I = np.zeros((5,5))
+        I[1,1] = 1.0; I[2,2] = 3.0
+        Iflat = np.ravel(I)
+        idx = 0
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [1, 5, 6])
+        assert np.all(vals == [0., 0., 1.])
+
+    def test_neighbors_flatlast(self):
+        I = np.zeros((5,5))
+        I[4,3] = 3.0
+        Iflat = np.ravel(I)
+        idx = 22
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [16, 17, 18, 21, 23])
+        assert np.all(vals == [0., 0., 0., 0., 3.])
+
+    def test_neighbors_flattop(self):
+        I = np.zeros((5,5))
+        I[0,3] = 1.0; I[1,4] = 3.0
+        Iflat = np.ravel(I)
+        idx = 4
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [3, 8, 9])
+        assert np.all(vals == [1., 0., 3.])
+
+    def test_neighbors_flatbottom(self):
+        I = np.zeros((5,5))
+        I[3,4] = 1.0
+        Iflat = np.ravel(I)
+        idx = 24
+        ncols = 5
+        filt='none'
+        idcs, vals = im_utils.neighbors_flat(idx, Iflat, ncols, filt=filt)
+        # make assertion
+        assert np.all(idcs == [18, 19, 23])
+        assert np.all(vals == [0., 1., 0.])
+
+class TestNeighbor_Vals:
+    """
+    Tests for the various conditions associated with the
+    im_utils.neighbor_vals function
+    """
+
+    def test_neighbor_zeros(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 0, 0)
+        # make assertion
+        assert vals[4] == 0.
+        assert vals[6] == 0.
+        assert vals[7] == 0.
+
+    def test_neighbor_midy(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 0, 2)
+        # make assertion
+        assert vals[1] == 0.
+        assert vals[2] == 0.
+        assert vals[4] == 0.
+        assert vals[6] == 0.
+        assert vals[7] == 0.
+
+    def test_neighbor_endy(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 0, 4)
+        # make assertion
+        assert vals[1] == 0.
+        assert vals[2] == 0.
+        assert vals[4] == 0.
+
+    def test_neighbor_endx(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 4, 0)
+        # make assertion
+        assert vals[3] == 0.
+        assert vals[5] == 0.
+        assert vals[6] == 0.
+
+    def test_neighbor_endxy(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 4, 4)
+        # make assertion
+        assert vals[0] == 0.
+        assert vals[1] == 0.
+        assert vals[3] == 0.
+
+    def test_neighbor_endxmid(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 4, 2)
+        # make assertion
+        assert vals[0] == 0.
+        assert vals[1] == 0.
+        assert vals[3] == 0.
+        assert vals[5] == 0.
+        assert vals[6] == 0.
+
+    def test_neighbor_endyzero(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 2, 0)
+        # make assertion
+        assert vals[3] == 0.
+        assert vals[4] == 0.
+        assert vals[5] == 0.
+        assert vals[6] == 0.
+        assert vals[7] == 0.
+
+    def test_neighbor_endymidx(self):
+        im = np.zeros((5,5))
+        vals = im_utils.neighbor_vals(im, 2, 4)
+        # make assertion
+        assert vals[0] == 0.
+        assert vals[1] == 0.
+        assert vals[2] == 0.
+        assert vals[3] == 0.
+        assert vals[4] == 0.
+
+class TestRemoveBlobs:
+    """
+    Tests for im_utils.largest_blobs
+    """
+
+    def test_remove_blobs(self):
+        I = np.zeros((5,5))
+        I[1:2,1] = 1
+        Ic = im_utils.remove_blobs(I, 3, connectivity=2)
+        # returned array should be all 0s
+        assert np.all(Ic==0)
+
+    def test_largest_blob_removal(self):
+        I = np.zeros((5,5))
+        I[1:4,1:4] = 1
+        Ic = im_utils.largest_blobs(I, nlargest=1, action='remove')
+        # make assertion
+        assert np.all(Ic) == 0
+
+    def test_largest_blob_error(self):
+        I = np.zeros((5,5))
+        I[1:4,1:4] = 1
+        Ic = im_utils.largest_blobs(I, nlargest=1, action='invalid')
+        # make assertion
+        assert np.all(Ic == I)
+
+class TestErode:
+    """
+    Tests for im_utils.erode
+    """
+
+    def test_erode_no_n(self):
+        I = np.zeros((3,3))
+        Ie = im_utils.erode(I,n=0,strel='square')
+        # make assertion
+        assert np.all(I==Ie)
+
+    def test_erode_square(self):
+        I = np.zeros((8,8))
+        I[1:7,1:7] = 1
+        Ie = im_utils.erode(I, n=1, strel='square')
+        # make assertion
+        assert np.all(Ie == np.array([[0., 0., 0., 0., 0., 0., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.],
+                                      [0., 0., 1., 1., 1., 1., 0., 0.],
+                                      [0., 0., 1., 1., 1., 1., 0., 0.],
+                                      [0., 0., 1., 1., 1., 1., 0., 0.],
+                                      [0., 0., 1., 1., 1., 1., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.]]))
+
+    def test_erode_plus(self):
+        I = np.zeros((8,8))
+        I[0:7,1:3] = 1; I[2:5,1:7] = 1
+        Ie = im_utils.erode(I, n=1, strel='plus')
+        # make assertion
+        assert np.all(Ie == np.array([[0., 0., 0., 0., 0., 0., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.],
+                                      [0., 0., 1., 0., 0., 0., 0., 0.],
+                                      [0., 0., 1., 1., 1., 1., 0., 0.],
+                                      [0., 0., 1., 0., 0., 0., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.]]))
+
+    def test_erode_disk(self):
+        I = np.zeros((8,8))
+        I[1:7,1:7] = 1
+        Ie = im_utils.erode(I, n=1, strel='disk')
+        # make assertion
+        assert np.all(Ie == np.zeros_like(Ie))
+
+class TestDilate:
+    """
+    Tests for im_utils.dilate
+    """
+
+    def test_dilate_no_n(self):
+        I = np.zeros((5,5))
+        Id = im_utils.dilate(I, n=0)
+        # do assertion
+        assert np.all(I == Id)
+
+    def test_dilate_square(self):
+        I = np.zeros((5,5))
+        I[2,2] = 1
+        Id = im_utils.dilate(I,n=1,strel='square')
+        # do assertion
+        assert np.all(Id == np.array([[0., 0., 0., 0., 0.],
+                                      [0., 1., 1., 1., 0.],
+                                      [0., 1., 1., 1., 0.],
+                                      [0., 1., 1., 1., 0.],
+                                      [0., 0., 0., 0., 0.]]))
+
+    def test_dilate_plus(self):
+        I = np.zeros((5,5))
+        I[2,2] = 1
+        Id = im_utils.dilate(I,n=1,strel='plus')
+        # do assertion
+        assert np.all(Id == np.array([[0., 0., 0., 0., 0.],
+                                      [0., 0., 1., 0., 0.],
+                                      [0., 1., 1., 1., 0.],
+                                      [0., 0., 1., 0., 0.],
+                                      [0., 0., 0., 0., 0.]]))
+
+    def test_dilate_disk(self):
+        I = np.zeros((8,8))
+        I[3,3] = 1
+        Id = im_utils.dilate(I,n=1,strel='disk')
+        # do assertion
+        assert np.all(Id == np.array([[0., 0., 0., 1., 0., 0., 0., 0.],
+                                      [0., 1., 1., 1., 1., 1., 0., 0.],
+                                      [0., 1., 1., 1., 1., 1., 0., 0.],
+                                      [1., 1., 1., 1., 1., 1., 1., 0.],
+                                      [0., 1., 1., 1., 1., 1., 0., 0.],
+                                      [0., 1., 1., 1., 1., 1., 0., 0.],
+                                      [0., 0., 0., 1., 0., 0., 0., 0.],
+                                      [0., 0., 0., 0., 0., 0., 0., 0.]]))
