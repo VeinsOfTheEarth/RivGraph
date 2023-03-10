VeinsOfTheEarth/RivGraph

commit 80986da79cbe89dd537e8e04365e3a2156fcf866

enabling single-thread meshing in core

rivgraph/classes.py
@@ -66,6 +66,11 @@ class rivnetwork:
         verbose : bool, optional
             If True, print run information and warnings to the console, default
             is False.
+        single_thread : bool, optional
+            If the input mask is mostly single-thread, set this to True to 
+            avoid problems in mesh generation later. If True, uses a different
+            mesh generation algorithm that is tailored to single-thread, 
+            meandering channels.
 
 
         Attributes
@@ -593,6 +598,7 @@ class delta(rivnetwork):
 
         """
         rivnetwork.__init__(self, name, path_to_mask, results_folder, verbose=verbose)
+        self.single_thread = False
 
 
     def skeletonize(self):
@@ -738,12 +744,13 @@ class river(rivnetwork):
     """
 
     def __init__(self, name, path_to_mask, results_folder=None,
-                 exit_sides=None, verbose=False):
+                 exit_sides=None, verbose=False, single_thread=False):
 
         if exit_sides is None:
             raise Warning('Must provide exit_sides for river class.')
 
         rivnetwork.__init__(self, name, path_to_mask, results_folder, exit_sides, verbose=verbose)
+        self.single_thread = single_thread
 
 
     def skeletonize(self):
@@ -794,7 +801,7 @@ class river(rivnetwork):
         logger.info('centerline computation is done.')
 
 
-    def compute_mesh(self, grid_spacing=None, smoothing=0.1, buf_halfwidth=None):
+    def compute_mesh(self, grid_spacing=None, smoothing=0.1, buf_halfwidth=None, single_thread=False):
         """
         Generates an along-centerline mesh that indicates a valley-direction
         of sorts. The mesh is useful for computing spatial statistics as a function
@@ -815,8 +822,13 @@ class river(rivnetwork):
             of the total centerline length. Range is [0, 1].
         buf_halfwidth : float
             Defines the offset distance of the left- and right-valleylines from
-            from the centerline. Units correspond to those of the CRS of the
+            from the centerline. buf_halfwidth * 2 is the length of each 
+            transect. Units correspond to those of the CRS of the
             input mask.
+        single_thread : bool
+            If True, uses a different mesh computing algorithm that is better-
+            suited for following single-thread channels (as opposed to a
+            multithread channel for which a valley-wide mesh must be computed).
 
         """
         # Need a centerline
@@ -850,10 +862,16 @@ class river(rivnetwork):
 
             # Multiply by pixlen to keep units consistent
             buf_halfwidth = self.max_valley_width_pixels * self.pixlen * 1.1
+            
+        if self.single_thread is True:
+            single_thread = True
 
         logger.info('Generating mesh...')
 
-        self.meshlines, self.meshpolys, self.centerline_smooth = ru.valleyline_mesh(self.centerline, self.avg_chan_width, buf_halfwidth, grid_spacing, smoothing=smoothing)
+        if single_thread is False:
+            self.meshlines, self.meshpolys, self.centerline_smooth = ru.valleyline_mesh(self.centerline, self.avg_chan_width, buf_halfwidth, grid_spacing, smoothing=smoothing)
+        else:
+            self.meshlines, self.meshpolys, self.centerline_smooth = ru.centerline_mesh(self.centerline, self.avg_chan_width, buf_halfwidth*2, grid_spacing, smoothing_param=smoothing)
 
         logger.info('mesh generation is done.')
 

rivgraph/rivers/river_utils.py
@@ -346,6 +346,30 @@ def mask_to_centerline(Imask, es):
     return coords, pix_width
 
 
+def mirror_line_ends(xs, ys, npad):
+    """
+    Reflect both ends of a line.
+
+    Reflects both ends of a line defined by x and y coordinates. The mirrored
+    distance is set by npad, which refers to the number of vertices along the
+    line to mirror.
+
+    """
+    # Mirror the beginning of the line
+    diff_x = np.diff(xs[0:npad])
+    xs_m = np.concatenate((np.flipud(xs[1] - np.cumsum(diff_x)), xs))
+    diff_y = np.diff(ys[0:npad])
+    ys_m = np.concatenate((np.flipud(ys[1] - np.cumsum(diff_y)), ys))
+
+    # Mirror the end of the line
+    diff_x = np.diff(xs[-npad:][::-1])
+    xs_m = np.concatenate((xs_m, xs_m[-1] - np.cumsum(diff_x)))
+    diff_y = np.diff(ys[-npad:][::-1])
+    ys_m = np.concatenate((ys_m, ys_m[-1] - np.cumsum(diff_y)))
+
+    return xs_m, ys_m
+
+
 def centerline_mesh(coords, width_chan, meshwidth, grid_spacing, smoothing_param=1):
     """
     Generate a centerline mesh.
@@ -361,17 +385,25 @@ def centerline_mesh(coords, width_chan, meshwidth, grid_spacing, smoothing_param
         2xN list, tuple, np.array (xs, ys) of coordinates defining centerline
     width_chan :
         width of the river in same units of coords
-    mesh_dist :
+    meshwidth :
         how wide should the mesh be, in same units of coords
     grid_spacing :
         how far apart should mesh cells be, in same units of coords
-
+        
+    Returns
+    -------
+    transects : list of shapely.LineStrings
+        the "perpendiculars" to the centerline used to generate the mesh
+    polys : list of shapely.Polygons
+        coordinates of the polygons representing the grid cells of the mesh
+    cl_smooth : shapely.LineString
+        the smoothed centerline used to compute transects
     """
-#    coords = ken.centerline
-#    width_chan = ken.width_chans
-#    meshwidth = ken.max_valley_width_pixels * ken.pixlen * 1.1
-#    grid_spacing = meshwidth/10
-#    smoothing_param = 1
+    # coords = alaska.centerline
+    # width_chan = alaska.avg_chan_width
+    # meshwidth = alaska.max_valley_width_pixels * alaska.pixlen * 1.1
+    # grid_spacing = meshwidth/2
+    # smoothing_param = 1
 
     if np.shape(coords)[0] == 2 and np.size(coords) != 4:
         coords = np.transpose(coords)
@@ -388,6 +420,7 @@ def centerline_mesh(coords, width_chan, meshwidth, grid_spacing, smoothing_param
     window_len = int(width_chan / np.mean(ds) * smoothing_param)
     if window_len % 2 == 0:  # Window must be odd
         window_len = window_len + 1
+    window_len = max(window_len, 5)
 
     # Smooth
     xs_sm = signal.savgol_filter(xs_m, window_length=window_len, polyorder=3,
@@ -467,39 +500,15 @@ def centerline_mesh(coords, width_chan, meshwidth, grid_spacing, smoothing_param
     # Build the polygon mesh
     polys = []
     for i in range(start_idx, end_idx+1):
-        polys.append([perp_aligned[i][0], perp_aligned[i][1],
+        polys.append(Polygon([perp_aligned[i][0], perp_aligned[i][1],
                       perp_aligned[i+1][1], perp_aligned[i+1][0],
-                      perp_aligned[i][0]])
+                      perp_aligned[i][0]]))
 
-    perps_out = perp_aligned[start_idx:end_idx+1]
-    cl_resampled = np.r_['1,2,0', xs_rs, ys_rs]
-    s_out = s[start_idx:end_idx+1] - s[start_idx]
+    # Convert the transects and smooth centerline
+    transects = [LineString(p) for p in perp_aligned[start_idx:end_idx+1]]
+    cl_smooth = LineString(zip(xs_sm, ys_sm))
 
-    return perps_out, polys, cl_resampled, s_out
-
-
-def mirror_line_ends(xs, ys, npad):
-    """
-    Reflect both ends of a line.
-
-    Reflects both ends of a line defined by x and y coordinates. The mirrored
-    distance is set by npad, which refers to the number of vertices along the
-    line to mirror.
-
-    """
-    # Mirror the beginning of the line
-    diff_x = np.diff(xs[0:npad])
-    xs_m = np.concatenate((np.flipud(xs[1] - np.cumsum(diff_x)), xs))
-    diff_y = np.diff(ys[0:npad])
-    ys_m = np.concatenate((np.flipud(ys[1] - np.cumsum(diff_y)), ys))
-
-    # Mirror the end of the line
-    diff_x = np.diff(xs[-npad:][::-1])
-    xs_m = np.concatenate((xs_m, xs_m[-1] - np.cumsum(diff_x)))
-    diff_y = np.diff(ys[-npad:][::-1])
-    ys_m = np.concatenate((ys_m, ys_m[-1] - np.cumsum(diff_y)))
-
-    return xs_m, ys_m
+    return transects, polys, cl_smooth
 
 
 def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
@@ -508,7 +517,7 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
     Generate a mesh over an input river centerline.
 
     This function generates a mesh over an input river centerline. The mesh
-    is generated across the valley, not just the channel width, in order to
+    is generated across the valley, not just the channel extents, in order to
     perform larger-scale spatial analyses. With the correct parameter
     combinations, it can also be used to generate a mesh for smaller-scale
     analysis, but it is optimized for larger and strange behavior may occur.
@@ -535,11 +544,12 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
 
     Returns
     -------
-    lines :
+    transects : list of shapely.LineStrings
         the "perpendiculars" to the centerline used to generate the mesh
-    polys :
+    polys : list of shapely.Polygons
         coordinates of the polygons representing the grid cells of the mesh
-
+    cl_smooth : shapely.LineString
+        the smoothed centerline used to compute transects
     """
 
     def find_cl_intersection_pts_and_distance(endpts, cl):
@@ -589,11 +599,9 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
 
 
     def iterative_cl_pt_mapping(cl, bufdists, side):
-
+        
         mapper = []
         lines = []
-        plt.close()
-
         old = cl
         for i, bd in enumerate(bufdists):
 
@@ -612,7 +620,7 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
             lines.append(new)
 
             old = new
-
+            
         return lines, mapper
 
 
@@ -717,7 +725,7 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
         return(xs_o2, ys_o2)
 
     """ Main function code begins here """
-    # obj = ind
+    # obj = alaska
     # coords = obj.centerline
     # avg_chan_width = obj.avg_chan_width
     # buf_halfwidth = obj.max_valley_width_pixels * obj.pixlen * 1.1
@@ -737,6 +745,7 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
     window_len = int(min(len(xs_o)/5, window_len))  # Smoothing window cannot be longer than 1/5 the centerline
     if window_len % 2 == 0:  # Window must be odd
         window_len = window_len + 1
+    window_len = max(5, window_len) # must be at least 5 else savgol_filter will fail
 
     # Extend the centerline ends to avoid boundary effects; we'll clip them later
     xs_o2, ys_o2 = mirror_lines(xs_o, ys_o, window_len)
@@ -746,6 +755,11 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
                                  mode='interp')
     ys_sm = signal.savgol_filter(ys_o2, window_length=window_len, polyorder=3,
                                  mode='interp')
+
+    # plt.close('all')
+    # plt.plot(xs_o, ys_o)
+    # plt.plot(xs_sm, ys_sm)
+    # plt.axis('equal')
 
     # Create shapely LineString centerline
     cl = LineString([(x, y) for x, y in zip(xs_sm, ys_sm)])
@@ -806,8 +820,8 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
     # clip the transects to only those that are needed
     cl_orig = LineString(zip(xs_o, ys_o))
     intersects_cl = [LineString(t).intersects(cl_orig) for t in transects]
-    first_idx = np.argmax(intersects_cl) - 1
-    last_idx = len(intersects_cl) - np.argmax(intersects_cl[::-1]) - 1 + 1  # -1/+1 for explicitness
+    first_idx = max(0,np.argmax(intersects_cl) - 1)
+    last_idx = min(len(intersects_cl) - np.argmax(intersects_cl[::-1]) - 1 + 1, len(transects)-1)  # -1/+1 for explicitness
     transects = [transects[i] for i in range(first_idx, last_idx + 1)]
 
     # Create mesh polygons

commit 6075e85939f6aceb142dcaf92967535a11141a00

fixing what i broke in https://github.com/VeinsOfTheEarth/RivGraph/commit/80986da79cbe89dd537e8e04365e3a2156fcf866

rivgraph/classes.py
@@ -882,7 +882,7 @@ class river(rivnetwork):
         channel network.
 
         """
-        if 'inlets' not in self.nodes.keys():
+        if hasattr(self, 'nodes') is False or 'inlets' not in self.nodes.keys():
             raise AttributeError('Cannot assign flow directions until prune_network() has been run.')
 
         if hasattr(self, 'centerline') is False:

rivgraph/rivers/river_directionality.py
@@ -541,6 +541,7 @@ def dir_centerline(links, nodes, meshpolys, meshlines, Imask, gt, pixlen):
 
     """
     # alg = 20
+    
     alg = dy.algmap('cl_dist_guess')
 
     # Create geodataframes for intersecting meshpolys with nodes

rivgraph/rivers/river_utils.py
@@ -579,7 +579,11 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
 
             # Project the intersection point to the centerline and return
             # the along-centerline distance of this point
-            dist_to_int.append(float(cl.project(int_pt)))
+            projpt = float(cl.project(int_pt))
+            if projpt == -1: # This catches GEOS Runtime errors (return -1s)
+                dist_to_int.append(None)
+            else:
+                dist_to_int.append(float(cl.project(int_pt)))
 
             # int_pts.append(int_pt)
 
@@ -725,7 +729,7 @@ def valleyline_mesh(coords, avg_chan_width, buf_halfwidth, grid_spacing,
         return(xs_o2, ys_o2)
 
     """ Main function code begins here """
-    # obj = alaska
+    # obj = test_river
     # coords = obj.centerline
     # avg_chan_width = obj.avg_chan_width
     # buf_halfwidth = obj.max_valley_width_pixels * obj.pixlen * 1.1

tests/integration/test_river_int.py
@@ -149,18 +149,17 @@ def test_centerline_mesh(test_river):
     avg_chan_width = np.sum(test_river.Imask) * test_river.pixarea / np.sum(test_river.links['len_adj'])
     mvw = ru.max_valley_width(test_river.Imask)
 
-    perps_out, polys, cl_resampled, s_out = ru.centerline_mesh(test_river.centerline,
+    perps_out, polys, cl_smoothed = ru.centerline_mesh(test_river.centerline,
                                             avg_chan_width,
                                             mvw*test_river.pixlen*1.1,
                                             mvw*test_river.pixlen*1.1/10,
                                             1)
 
     # make assertions
-    assert np.shape(perps_out) == (86, 2, 2)
-    assert np.shape(polys) == (86, 5, 2)
-    assert np.shape(cl_resampled) == (95, 2)
-    assert np.shape(s_out) == (86,)
-    assert s_out[-1] == pytest.approx(134836.27933429673)
+    assert np.shape(perps_out) == (86,)
+    assert np.shape(polys) == (86,)
+    assert len(cl_smoothed.xy[0]) == 4721
+    assert cl_smoothed.length == pytest.approx(151134.13414783287)
 
 
 def test_river_dirs(tmp_path):